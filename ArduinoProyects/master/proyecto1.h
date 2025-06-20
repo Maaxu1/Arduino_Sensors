9dc6a1310af339a71ade8d4b73f5c9bb51360d4b
Maaxu1 <max05gonz@gmail.com>
2025-06-20 15:12:51 -0500

    Delete ArduinoProyects directory

--- ArduinoProyects/master/proyecto1.h
+++
@@ -1,8 +0,0 @@
-#ifndef PROYECTO1_H
-#define PROYECTO1_H
-
-void proyecto1Setup();
-void proyecto1();
-void proyecto1Reset();
-
-#endif
--- ArduinoProyects/master/proyecto5.h
+++
@@ -1,8 +0,0 @@
-#ifndef PROYECTO5_H
-#define PROYECTO5_H
-
-void proyecto5Setup();
-void proyecto5();
-void proyecto5Reset();
-
-#endif
